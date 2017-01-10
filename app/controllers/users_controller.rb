2f91bb5d883490d5b55f897b18553e5ad48b28aa
Shusaku Hotta <pentar23@gmail.com>
2017-01-10 11:14:48 +0000

    add profile_location

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+
+  def show # 追加
+   @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+  end
+
+  def update
+    if params.update(user_params)
+      redirect_to @user , notice: '更新しました'
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation, 
+                                 :profile, :location)
+  end
+end
